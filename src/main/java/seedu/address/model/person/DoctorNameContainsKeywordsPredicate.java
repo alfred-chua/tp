82f253c062555a8a5664ce304e7790b304540e41
alfred-chua <e1406317@u.nus.edu>
2025-10-23 13:35:30 +0800

    Fix JavaCI issue

--- src/main/java/seedu/address/model/person/DoctorNameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/DoctorNameContainsKeywordsPredicate.java
@@ -33,8 +33,8 @@ public class DoctorNameContainsKeywordsPredicate implements Predicate<Person> {
             return false;
         }
 
-        DoctorNameContainsKeywordsPredicate otherNameContainsKeywordsPredicate
-                = (DoctorNameContainsKeywordsPredicate) other;
+        DoctorNameContainsKeywordsPredicate otherNameContainsKeywordsPredicate =
+                (DoctorNameContainsKeywordsPredicate) other;
         return keywords.equals(otherNameContainsKeywordsPredicate.keywords);
     }
 
