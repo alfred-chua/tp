60e694bdccb2eeae63cf15dd13d61c7bc82d04ac
alfred-chua <e1406317@u.nus.edu>
2025-10-23 20:54:04 +0800

    Change Javadoc comment

--- src/main/java/seedu/address/model/person/DoctorNameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/DoctorNameContainsKeywordsPredicate.java
@@ -7,7 +7,7 @@ import seedu.address.commons.util.StringUtil;
 import seedu.address.commons.util.ToStringBuilder;
 
 /**
- * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
+ * Tests that a {@code Person}'s doctor's {@code Name} matches any of the keywords given.
  */
 public class DoctorNameContainsKeywordsPredicate implements Predicate<Person> {
     private final List<String> keywords;
